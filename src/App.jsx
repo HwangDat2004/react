a06f8c7ec2ae5060e8088a4140ae8eb3cf1cf846
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2024-10-13 17:03:28 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+
 import "./App.scss";
-import AdminLayout from "./layouts/AdminLayout";
-import Dashboard from "./pages/Dashboard";
-import ProductManagement from "./pages/ProductManagement";
-import AddProduct from "./pages/AddProduct";
 
 function App() {
 	return (
